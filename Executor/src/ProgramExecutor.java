f75bfc8d7a17621d7d8571040f44bd5bf27aea25
xoh <1270218405@qq.com>
2019-04-14 10:38:57 +0800

    start executor

--- Executor/src/ProgramExecutor.java
+++ Executor/src/ProgramExecutor.java
@@ -1,3 +1,29 @@
-public class ProgramExecutor {
+import java.util.Stack;
 
+public class ProgramExecutor {
+    CNode root;
+    XEnv runEnv;
+    CNode pointer;
+    Stack<CNode> nodeStack;
+    int statusNum = 0;
+
+    public ProgramExecutor(CNode root) {
+        this.root = root;
+        runEnv = new XEnv(null);
+        pointer = root;
+        nodeStack = new Stack<>();
+    }
+
+    public int run() {
+        if (root.production == GrammarEnum.Program_To_ExtDefList) {
+
+        } else {
+
+        }
+        return statusNum;
+    }
+
+    public void extDefList() {
+
+    }
 }
